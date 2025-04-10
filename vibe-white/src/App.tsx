3bf2528670304da471702f84c239226bd03605be
vincent <yoyo12825@gmail.com>
2025-04-10 14:47:40 +0800

    測試git push

---
+++ vibe-white/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './index.css'
+import Home from './pages/Home.tsx';
+
+function App() {
+  const [, ] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Home />
+      </div>
+    </>
+  )
+}
+
+export default App
