beeb8dae62600f91b03f770850264595d5dfde40
vincent <yoyo12825@gmail.com>
2025-04-14 15:03:40 +0800

    add Footer component

--- vibe-white/src/App.tsx
+++ vibe-white/src/App.tsx
@@ -1,5 +1,6 @@
 import './index.css'
 import Home from './pages/Home.tsx';
+import Footer from './components/Footer.tsx';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <div>
         <Home />
       </div>
+      <Footer />
     </>
   )
 }
---
+++ vibe-white/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react";
+
+const Footer: FC = () => {
+  return (
+    <div className="bg-olive-200 fixed right-0 bottom-0 left-0 z-50 mx-3 h-14 rounded-t-2xl p-1 shadow-2xl">
+      <div className="flex"></div>
+    </div>
+  );
+};
+
+export default Footer;
