4f53534555dea7516d13722ccee1a1708d4a9bb7
vincent <yoyo12825@gmail.com>
2025-04-22 14:48:27 +0800

    adjust css

--- vibe-white/src/components/Header.tsx
+++ vibe-white/src/components/Header.tsx
@@ -2,13 +2,13 @@ import { FC } from "react";
 
 const Header: FC = () => {
   return (
-    <header className="flex w-full z-10">
+    <header className="flex w-full z-10 fade-down">
       <div className="relative w-full">
         <img src="/images/logo.svg" alt="" className="absolute top-2 left-2" />
         <ul className="black-ops mt-2 flex justify-center text-5xl font-bold pointer-events-none">
           <div className="title-typing flex gap-5">
-            <p className="text-olive-400">VIBE</p>
-            <p className="text-white">WHITE</p>
+            <p className="text-olive-400">DEFINE YOUR OWN</p>
+            <p className="text-white">VIBE</p>
           </div>
         </ul>
       </div>
--- vibe-white/src/components/LoadingAnimation.tsx
+++ vibe-white/src/components/LoadingAnimation.tsx
@@ -1,6 +1,6 @@
 import { FC, useEffect } from "react";
 import { createTimeline, stagger } from "animejs";
-
+import '../assets/style/Header.css'
 interface LoadingAnimationProps {
   onComplete: () => void;
 }
@@ -9,17 +9,33 @@ const LoadingAnimation: FC<LoadingAnimationProps> = ({ onComplete }) => {
   const letters = ['V', 'I', 'B', 'E', 'W', 'H', 'I', 'T', 'E'];
   useEffect(() => {
     const tl = createTimeline({ defaults: { duration: 750 }, onComplete: () => onComplete?.() });
+    // 向下淡入
     tl.add('.logo-letter', {
       opacity: [0, 1],
       y: '2rem',
       delay: stagger(200),
     }, 1000)
-      .add('.logo-letter', {
-        opacity: [1, 0],
-        scale: 20,
-        ease: 'inSine',
-        delay: stagger(50),
-      }, '+=1000')
+    // 淡出非Logo字母
+    .add('.minor-letter', {
+      opacity: [1, 0],
+      duration: 1500
+    }, '+=500')
+    // 組合Logo1
+    .add('.main-letter-1', {
+      x: '4rem',
+      scale: 2
+    }, '+=100')
+    // 組合Logo2
+    .add('.main-letter-2', {
+      x: '-1.5rem',
+      scale: 2
+    }, '+=100')
+    // 放大淡出
+    .add('.main-letter-2, .main-letter-1', {
+      opacity: [1, 0],
+      scale: 20,
+      duration: 1000
+    }, '+=1000')
   }, [onComplete]);
 
   return (
@@ -27,14 +43,14 @@ const LoadingAnimation: FC<LoadingAnimationProps> = ({ onComplete }) => {
       <h1 className="text-5xl font-bold text-white flex mt-2 gap-5">
         <div>
           {letters.slice(0, 4).map((char, index) => (
-            <span key={index} className="logo-letter opacity-0 black-ops text-olive-400 inline-block">
+            <span key={index} className={'logo-letter opacity-0 black-ops text-olive-400 inline-block' + (index === 0 ? ' main-letter-1' : ' minor-letter') }>
               {char}
             </span>
           ))}
         </div>
         <div>
           {letters.slice(4).map((char, index) => (
-            <span key={index} className="logo-letter opacity-0 black-ops text-white inline-block">
+            <span key={index} className={'logo-letter opacity-0 black-ops text-white inline-block' + (index === 0 ? ' main-letter-2' : ' minor-letter') }>
               {char}
             </span>
           ))}
